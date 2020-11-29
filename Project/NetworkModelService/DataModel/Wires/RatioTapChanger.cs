343454b70dac39008a0ecaa31122687606e21f28
Danilo Novakovic <dakenzi97@gmail.com>
2020-11-29 16:17:13 +0100

    Added copy constructor for rest of DataModel classes

--- Project/NetworkModelService/DataModel/Wires/RatioTapChanger.cs
+++ Project/NetworkModelService/DataModel/Wires/RatioTapChanger.cs
@@ -1,28 +1,27 @@
 ﻿using FTN.Common;
 using FTN.Services.NetworkModelService.DataModel.Core;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace FTN.Services.NetworkModelService.DataModel.Wires
 {
     public class RatioTapChanger : TapChanger
     {
-        private long transformerWinding = 0;
+        public long TransformerWinding { get; set; } = 0;
 
         public RatioTapChanger(long gID) : base(gID)
         {
         }
-
-        public long TransformerWinding { get => transformerWinding; set => transformerWinding = value; }
+        public RatioTapChanger(RatioTapChanger tapChanger) : base(tapChanger)
+        {
+            TransformerWinding = tapChanger.TransformerWinding;
+        }
 
         public override bool Equals(object obj)
         {
             if (base.Equals(obj))
             {
                 RatioTapChanger x = (RatioTapChanger)obj;
-                return ((x.transformerWinding == this.transformerWinding));
+                return ((x.TransformerWinding == this.TransformerWinding));
             }
             else
             {
@@ -36,6 +35,19 @@ namespace FTN.Services.NetworkModelService.DataModel.Wires
         }
 
         #region IAccess
+        public override void GetProperty(Property property)
+        {
+            switch (property.Id)
+            {
+                case ModelCode.RATIOTAPCHANGER_TRWINDING:
+                    property.SetValue(TransformerWinding);
+                    break;
+                default:
+                    base.GetProperty(property);
+                    break;
+            }
+        }
+
         public override bool HasProperty(ModelCode property)
         {
             switch (property)
@@ -46,26 +58,12 @@ namespace FTN.Services.NetworkModelService.DataModel.Wires
                     return base.HasProperty(property);
             }
         }
-
-        public override void GetProperty(Property property)
-        {
-            switch (property.Id)
-            {
-                case ModelCode.RATIOTAPCHANGER_TRWINDING:
-                    property.SetValue(transformerWinding);
-                    break;
-                default:
-                    base.GetProperty(property);
-                    break;
-            }
-        }
-
         public override void SetProperty(Property property)
         {
             switch (property.Id)
             {
                 case ModelCode.RATIOTAPCHANGER_TRWINDING:
-                    transformerWinding = property.AsReference();
+                    TransformerWinding = property.AsReference();
                     break;
                 default:
                     base.SetProperty(property);
@@ -78,10 +76,10 @@ namespace FTN.Services.NetworkModelService.DataModel.Wires
 
         public override void GetReferences(Dictionary<ModelCode, List<long>> references, TypeOfReference refType)
         {
-            if (transformerWinding != 0 && (refType == TypeOfReference.Reference || refType == TypeOfReference.Both))
+            if (TransformerWinding != 0 && (refType == TypeOfReference.Reference || refType == TypeOfReference.Both))
             {
                 references[ModelCode.RATIOTAPCHANGER_TRWINDING] = new List<long>();
-                references[ModelCode.RATIOTAPCHANGER_TRWINDING].Add(transformerWinding);
+                references[ModelCode.RATIOTAPCHANGER_TRWINDING].Add(TransformerWinding);
             }
 
             base.GetReferences(references, refType);
--- Project/NetworkModelService/DataModel/Wires/TapChanger.cs
+++ Project/NetworkModelService/DataModel/Wires/TapChanger.cs
@@ -1,32 +1,32 @@
 ﻿using FTN.Common;
 using FTN.Services.NetworkModelService.DataModel.Core;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace FTN.Services.NetworkModelService.DataModel.Wires
 {
     public class TapChanger : PowerSystemResource
     {
-        private int highStep;
-        private int lowStep;
-        private int normalStep;
+        public int HighStep { get; set; }
+
+        public int LowStep { get; set; }
+
+        public int NormalStep { get; set; }
 
         public TapChanger(long gID) : base(gID)
         {
         }
-
-        public int HighStep { get => highStep; set => highStep = value; }
-        public int LowStep { get => lowStep; set => lowStep = value; }
-        public int NormalStep { get => normalStep; set => normalStep = value; }
+        public TapChanger(TapChanger tapChanger) : base(tapChanger)
+        {
+            HighStep = tapChanger.HighStep;
+            LowStep = tapChanger.LowStep;
+            NormalStep = tapChanger.NormalStep;
+        }
 
         public override bool Equals(object obj)
         {
             if (base.Equals(obj))
             {
                 TapChanger x = (TapChanger)obj;
-                return ((x.highStep == this.highStep) && (x.lowStep == this.lowStep) && (x.normalStep == this.normalStep));
+                return ((x.HighStep == this.HighStep) && (x.LowStep == this.LowStep) && (x.NormalStep == this.NormalStep));
             }
             else
             {
@@ -40,6 +40,26 @@ namespace FTN.Services.NetworkModelService.DataModel.Wires
         }
 
         #region IAccess
+        public override void GetProperty(Property property)
+        {
+            switch (property.Id)
+            {
+                case ModelCode.TAPCHANGER_HIGHSTEP:
+                    property.SetValue(HighStep);
+                    break;
+                case ModelCode.TAPCHANGER_LOWSTEP:
+                    property.SetValue(LowStep);
+                    break;
+                case ModelCode.TAPCHANGER_NORMALSTEP:
+                    property.SetValue(NormalStep);
+                    break;
+
+                default:
+                    base.GetProperty(property);
+                    break;
+            }
+        }
+
         public override bool HasProperty(ModelCode property)
         {
             switch (property)
@@ -53,37 +73,18 @@ namespace FTN.Services.NetworkModelService.DataModel.Wires
                     return base.HasProperty(property);
             }
         }
-        public override void GetProperty(Property property)
-        {
-            switch (property.Id)
-            {
-                case ModelCode.TAPCHANGER_HIGHSTEP:
-                    property.SetValue(highStep);
-                    break;
-                case ModelCode.TAPCHANGER_LOWSTEP:
-                    property.SetValue(lowStep);
-                    break;
-                case ModelCode.TAPCHANGER_NORMALSTEP:
-                    property.SetValue(normalStep);
-                    break;
-
-                default:
-                    base.GetProperty(property);
-                    break;
-            }
-        }
         public override void SetProperty(Property property)
         {
             switch (property.Id)
             {
                 case ModelCode.TAPCHANGER_HIGHSTEP:
-                    highStep = property.AsInt();
+                    HighStep = property.AsInt();
                     break;
                 case ModelCode.TAPCHANGER_LOWSTEP:
-                    lowStep = property.AsInt();
+                    LowStep = property.AsInt();
                     break;
                 case ModelCode.TAPCHANGER_NORMALSTEP:
-                    normalStep = property.AsInt();
+                    NormalStep = property.AsInt();
                     break;
 
                 default:
