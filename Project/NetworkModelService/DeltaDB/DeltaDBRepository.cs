860a67c129f3d6561494113ea4d6dae031884419
ZeljkaM <matoviczeljkaa@gmail.com>
2020-11-29 23:46:48 +0100

    DeltaDB added

---
+++ Project/NetworkModelService/DeltaDB/DeltaDBRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.ServiceModel;
+using FTN.Common;
+using System.Configuration;
+
+
+namespace FTN.Services.NetworkModelService.DeltaDB
+{
+	public class DeltaDBRepository : IDeltaDBRepository
+	{
+		private DeltaDBContext context = new DeltaDBContext();
+
+		public void AddDelta(DeltaDBModel delta)
+		{
+			context.Deltas.Add(delta);
+			context.SaveChanges();
+		}
+
+		public void DeleteAllDeltas()
+		{
+			context.Deltas.RemoveRange(context.Deltas);
+			context.SaveChanges();
+		}
+
+		public DeltaDBModel FindDeltaById(long id)
+		{
+			return context.Deltas.Find(id);
+		}
+
+		public List<DeltaDBModel> GetAllDeltas()
+		{
+			return context.Deltas.ToList();
+		}
+	}
+}
---
+++ Project/NetworkModelService/DeltaDB/IDeltaDBRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.ServiceModel;
+using FTN.Common;
+using System.Configuration;
+using Microsoft.SqlServer.TransactSql.ScriptDom;
+
+namespace FTN.Services.NetworkModelService.DeltaDB
+{
+    [ServiceContract]
+    public interface IDeltaDBRepository
+    {
+        [OperationContract]
+        void AddDelta(DeltaDBModel delta);
+
+        [OperationContract]
+        DeltaDBModel FindDeltaById(long id);
+
+        [OperationContract]
+        List<DeltaDBModel> GetAllDeltas();
+
+        [OperationContract]
+        void DeleteAllDeltas();
+    }
+}
