fbcd6a3fc52caa58c49c694904c36da6054aa10c
stanka14 <58149641+stanka14@users.noreply.github.com>
2020-12-08 18:11:41 +0100

    TMContracts

--- Project/TMContracts/IEnlistManager.cs
+++ Project/TMContracts/IEnlistManager.cs
@@ -1,13 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
 using System.Text;
+using System.Threading.Tasks;
 
-namespace Contracts
+namespace TMContracts
 {
+    [ServiceContract]
     public interface IEnlistManager
     {
+        [OperationContract]
         void Enlist();
 
+        [OperationContract]
         void EndEnlist(bool isSuccessful);
     }
 }
---
+++ Project/TMContracts/ITransactionSteps.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMContracts
+{
+    [ServiceContract]
+    public interface ITransactionSteps
+    {
+        [OperationContract]
+        bool Prepare();
+        [OperationContract]
+        bool Commit();
+        [OperationContract]
+        void Rollback();
+    }
+}
