b50d9e0ec28dd4a5d450f65ea69e379dc2f6300e
ZeljkaM <matoviczeljkaa@gmail.com>
2020-12-20 22:16:08 +0100

    GUI Views and ViewModels added

--- Project/GUI/ViewModels/MainWindowViewModel.cs
+++ Project/GUI/ViewModels/MainWindowViewModel.cs
@@ -3,10 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Caliburn.Micro;
 
 namespace GUI.ViewModels
 {
-    public class MainWindowViewModel
+    public class MainWindowViewModel : Conductor<object>
     {
+        public void LoadGraphicsView()
+        {
+            ActivateItem(new GraphicsViewModel());
+        }
+
+        public void LoadScadaDataView()
+        {
+            ActivateItem(new ScadaDataViewModel());
+        }
+
+        public void LoadDomView()
+        {
+            ActivateItem(new DOMViewModel());
+        }
+
+        public void LoadAlarmingView()
+        {
+            ActivateItem(new AlarmingViewModel());
+        }
+
+        public void LoadHistoryView()
+        {
+            ActivateItem(new HistoryViewModel());
+        }
+
+        public void CEDataView()
+        {
+            ActivateItem(new CEDataViewModel());
+        }
     }
 }
