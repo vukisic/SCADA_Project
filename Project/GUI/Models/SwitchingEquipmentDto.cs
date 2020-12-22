106c4370dec0081e208b48c672c6df3c87a45d04
stanka14 <58149641+stanka14@users.noreply.github.com>
2020-12-22 11:09:57 +0100

    DOMViewModel added

---
+++ Project/GUI/Models/SwitchingEquipmentDto.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+
+namespace GUI.Models
+{
+    public class SwitchingEquipmentDto : PropertyChangedBase
+    {
+        public SwitchingEquipmentDto() { }
+
+        #region Fields
+        private string mrid;
+        private int manipulationConut;
+        #endregion
+
+        #region Properties
+
+        public string Mrid
+        {
+            get { return this.mrid; }
+            set
+            {
+                this.mrid = value;
+                this.NotifyOfPropertyChange(() => this.Mrid);
+            }
+        }
+        public int ManipulationConut
+        {
+            get { return this.manipulationConut; }
+            set
+            {
+                this.manipulationConut = value;
+                this.NotifyOfPropertyChange(() => this.ManipulationConut);
+            }
+        }
+        #endregion
+    }
+}
--- Project/GUI/ViewModels/DOMViewModel.cs
+++ Project/GUI/ViewModels/DOMViewModel.cs
@@ -1,13 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Caliburn.Micro;
+using GUI.Models;
 
 namespace GUI.ViewModels
 {
-    public class DOMViewModel : Screen
+    public class DOMViewModel : Conductor<object>
     {
+        private ObservableCollection<SwitchingEquipmentDto> tableData;
+        public ObservableCollection<SwitchingEquipmentDto> TableData
+        {
+            get { return tableData; }
+            set
+            {
+                tableData = value;
+                NotifyOfPropertyChange(() => TableData);
+            }
+        }
+        public DOMViewModel()
+        {
+            tableData = new ObservableCollection<SwitchingEquipmentDto>();
+
+            SwitchingEquipmentDto first = new SwitchingEquipmentDto() { ManipulationConut = 3, Mrid = "id of first" };
+            SwitchingEquipmentDto sec = new SwitchingEquipmentDto() { ManipulationConut = 3, Mrid = "id of sec" };
+
+            tableData.Add(first);
+            tableData.Add(sec);
+        }
     }
 }
