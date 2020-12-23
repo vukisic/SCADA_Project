088400be5bfc55e646fa642f23c80f435e0a01a4
Vuk Isic <visicev@gmail.com>
2020-12-23 12:27:54 +0100

    Acquisitor-partial

---
+++ Project/FEP/ProcessingModule/Acquisitor.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FEP.ProcessingModule
+{
+    public class Acquisitor : IDisposable
+    {
+        private AutoResetEvent acquisitionTrigger;
+        private IProcessingManager processingManager;
+        private Thread acquisitionWorker;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Acquisitor"/> class.
+        /// </summary>
+        /// <param name="acquisitionTrigger">The acquisition trigger.</param>
+        /// <param name="processingManager">The processing manager.</param>
+        /// <param name="stateUpdater">The state updater.</param>
+		public Acquisitor(AutoResetEvent acquisitionTrigger, IProcessingManager processingManager)
+        {
+            this.acquisitionTrigger = acquisitionTrigger;
+            this.processingManager = processingManager;
+            this.InitializeAcquisitionThread();
+            this.StartAcquisitionThread();
+        }
+
+        #region Private Methods
+
+        /// <summary>
+        /// Initializes the acquisition thread.
+        /// </summary>
+        private void InitializeAcquisitionThread()
+        {
+            this.acquisitionWorker = new Thread(Acquisition_DoWork);
+            this.acquisitionWorker.Name = "Acquisition thread";
+        }
+
+        /// <summary>
+        /// Starts the acquisition thread.
+        /// </summary>
+		private void StartAcquisitionThread()
+        {
+            acquisitionWorker.Start();
+        }
+
+        /// <summary>
+        /// Acquisitor thread logic.
+        /// </summary>
+		private void Acquisition_DoWork()
+        {
+            try
+            {
+                while (true)
+                {
+                    acquisitionTrigger.WaitOne();
+                    // TO DO: Implement read class 0 in processing manager
+                    //processingManager.ExecuteReadCommand();
+                }
+            }
+            catch (Exception ex)
+            {
+                string message = $"{ex.TargetSite.ReflectedType.Name}.{ex.TargetSite.Name}: {ex.Message}";
+            }
+        }
+
+        #endregion Private Methods
+
+        /// <inheritdoc />
+        public void Dispose()
+        {
+            acquisitionWorker.Abort();
+        }
+    }
+}
