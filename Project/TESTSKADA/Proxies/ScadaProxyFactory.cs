fb75442ade1f40a4d2d4480cf36eb502b976b613
Vuk Isic <visicev@gmail.com>
2021-01-12 15:02:55 +0100

    Scada proxy factory added
    Factory for:
     - History proxy
     - DOM proxy
     - Logging proxy
     - Alarming&Kruncing proxy

---
+++ Project/TESTSKADA/Proxies/ScadaProxyFactory.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NDS.Proxies
+{
+    public class ScadaProxyFactory
+    {
+        private static ScadaProxyFactory instance;
+        private static object obj = new object();
+
+        private ScadaProxyFactory() { }
+
+        public static ScadaProxyFactory Instance()
+        {
+            lock (obj)
+            {
+                if (instance == null)
+                    instance = new ScadaProxyFactory();
+                return instance;
+            }
+        }
+
+        public AlarmKruncingProxy AlarmKruncingProxy()
+        {
+            return new AlarmKruncingProxy();
+        }
+
+        public DOMProxy DOMProxy()
+        {
+            return new DOMProxy();
+        }
+
+        public HistoryProxy HistoryProxy()
+        {
+            return new HistoryProxy();
+        }
+
+        public LoggingProxy LoggingProxy()
+        {
+            return new LoggingProxy();
+        }
+
+    }
+}
