c3ad240093241033a880200ec4f08a6650b7749e
stanka14 <58149641+stanka14@users.noreply.github.com>
2021-01-13 14:01:09 +0100

    GraphElement and Working Graph added

---
+++ Project/TESTCE/Data/GraphElement.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TESTCE.Data
+{
+    public class GrpahElement
+    {
+        public List<long> XAxe { get; set; }
+        public List<DateTime> YAxe { get; set; }
+    }
+}
---
+++ Project/TESTCE/Data/WorkingGraph.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TESTCE.Data
+{
+    public class WorkingGraph
+    {
+        public GrpahElement Pump1 { get; set; }
+        public GrpahElement Pump2 { get; set; }
+        public GrpahElement Pump3 { get; set; }
+    }
+}
