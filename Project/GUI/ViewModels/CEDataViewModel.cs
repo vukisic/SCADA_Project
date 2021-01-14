87d6571385fb91450f0a83e3981a1c30d3c7be10
Vuk Isic <visicev@gmail.com>
2021-01-14 23:44:56 +0100

    Remaining NServiceBus interfaces on GUI are now implemented

--- Project/GUI/ViewModels/CEDataViewModel.cs
+++ Project/GUI/ViewModels/CEDataViewModel.cs
@@ -4,10 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Caliburn.Micro;
+using Core.Common.ServiceBus.Commands;
+using Core.Common.ServiceBus.Events;
 
 namespace GUI.ViewModels
 {
     public class CEDataViewModel : Screen
     {
+        internal void Update(object sender, CeUpdateEvent e)
+        {
+            App.Current.Dispatcher.Invoke((System.Action)delegate
+            {
+                // Update code
+            });
+        }
     }
 }
