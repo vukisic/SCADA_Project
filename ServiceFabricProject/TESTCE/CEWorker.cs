60ab1d27e173de2400d591cdb001c45bb22d6dfd
stanka14 <58149641+stanka14@users.noreply.github.com>
2021-03-26 12:05:37 +0100

    CEWroker changed

--- ServiceFabricProject/TESTCE/CEWorker.cs
+++ ServiceFabricProject/TESTCE/CEWorker.cs
@@ -16,29 +16,39 @@ namespace CE
 {
     public class CEWorker : IDisposable
     {
+        private long seconds = 0;
+        //private long pump1Time;
+        //private long pump2Time;
+        //private long pump3Time;
+
         private IFitnessFunction algorithm;
         private Thread _worker;
+        public EventHandler<int> _updateEvent = delegate { };
         private bool pointUpdateOccures;
         private bool endFlag;
         private int points = 0;
-        private WeatherServiceProxy weatherAPI;
-        public int TPoints = 0;
+        private WeatherAPI weatherAPI;
         private IEndpointInstance endpoint;
         private static bool skip = false;
+        private int secundsForWeather = 60;
+        private int hourIndex = 0;
+        private int hourIndexChanged = 0;
+        private CeGraphicalEvent graph;
 
         private DNA<float> result;
 
         public CEWorker()
         {
-            //endpoint = ServiceBusStartup.StartInstance("CE").GetAwaiter().GetResult();
+            _updateEvent += OnPointUpdate;
+            endpoint = ServiceBusStartup.StartInstance("CE").GetAwaiter().GetResult();
         }
 
         public void Start()
         {
-            _worker = new Thread(DoWork);
+            _worker = new Thread(DoWorkNew);
             endFlag = true;
             _worker.Name = "CE Worker";
-            weatherAPI = new WeatherServiceProxy();
+            weatherAPI = new WeatherAPI();
             _worker.Start();
         }
 
@@ -48,49 +58,185 @@ namespace CE
             _worker.Abort();
             _worker = null;
         }
-
-        private void DoWork()
+        private void DoWorkNew()
         {
+            graph = new CeGraphicalEvent();
+            graph.PumpsValues = new Core.Common.ServiceBus.Events.CeGraph();
             while (endFlag)
             {
-                if (points > 0 && points < 4)
+                try
                 {
-                    if (pointUpdateOccures)
+                    if (hourIndexChanged == 3600)
                     {
-                        ChangeStrategy();
+                        hourIndex++;
                     }
 
-
-                    var forecastResult = new CeForecast();
-                    var area = GetSurfaceArea();
-                    var weatherForecast = weatherAPI.GetResultsForNext6Hours();
-                    var weather = new List<double>();
-                    weatherForecast.ForEach(x => weather.Add(x * area));
-                    float current = GetCurrentFluidLevel();
-
-                    for (int i = 0; i < weatherForecast.Count; i++)
+                    if (seconds == 10800 || seconds == 0 || pointUpdateOccures)
                     {
-                        current += (float)weather[i];
-                        if (skip && i==0)
+                        // 3hrs
+                        Calculations();
+                        seconds = 0;
+                    }
+                    CheckState();
+
+                    // Sleep for 10s
+                    Thread.Sleep(10000);
+                    // Add 10s to seconds
+                    seconds += 10;
+                    secundsForWeather += 10;
+                    hourIndexChanged += 10;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+        private void CheckState()
+        {
+            ScadaExportProxy proxy = new ScadaExportProxy();
+            var measurements = proxy.GetData();
+
+            if (!measurements.ContainsKey("FluidLevel_Tank"))
+                return;
+            var fluidLevel = measurements["FluidLevel_Tank"] as AnalogPoint;
+
+            if (LevelIsOptimal(fluidLevel.Value))
+            {
+                // SendCommands To TurOff Breakers for pumps
+                TurnOffPumps();
+            }
+
+            //graph.PumpsValues.Pump1.XAxes.Add(DateTime.Now);
+            //graph.PumpsValues.Pump1.YAxes.Add(pump1Time);
+            //if (measurements.ContainsKey("Flow_AM1"))
+            //{
+            //    var flow1 = measurements["Flow_AM1"] as AnalogPoint;
+            //    if (flow1 != null && flow1.Value > 0)
+            //        pump1Time += 10;
+            //}
+
+
+            //graph.PumpsValues.Pump2.XAxes.Add(DateTime.Now);
+            //graph.PumpsValues.Pump2.YAxes.Add(pump2Time);
+            //if (measurements.ContainsKey("Flow_AM2"))
+            //{
+            //    var flow2 = measurements["Flow_AM2"] as AnalogPoint;
+            //    if (flow2 != null && flow2.Value > 0)
+            //        pump2Time += 10;
+            //}
+
+            //graph.PumpsValues.Pump3.XAxes.Add(DateTime.Now);
+            //graph.PumpsValues.Pump3.YAxes.Add(pump3Time);
+            //if (measurements.ContainsKey("Flow_AM3"))
+            //{
+            //    var flow3 = measurements["Flow_AM3"] as AnalogPoint;
+            //    if (flow3 != null && flow3.Value > 0)
+            //        pump3Time += 10;
+            //}
+
+            //endpoint.Publish(graph).ConfigureAwait(false).GetAwaiter().GetResult();
+        }
+
+        private void TurnOffPumps()
+        {
+
+            ScadaExportProxy proxy = new ScadaExportProxy();
+            var points = proxy.GetData();
+
+            if (points.ContainsKey("Breaker_21Status"))
+            {
+                var breaker1 = points["Breaker_21Status"] as DiscretePoint;
+                var command1 = new ScadaCommandingEvent()
+                {
+                    Index = (uint)breaker1.Index,
+                    RegisterType = breaker1.RegisterType,
+                    Value = 0,
+                    Milliseconds = 0
+                };
+                endpoint.Publish(command1).ConfigureAwait(false);
+            }
+
+            if (points.ContainsKey("Breaker_21Status"))
+            {
+                var breaker2 = points["Breaker_22Status"] as DiscretePoint;
+                var command2 = new ScadaCommandingEvent()
+                {
+                    Index = (uint)breaker2.Index,
+                    RegisterType = breaker2.RegisterType,
+                    Value = 0,
+                    Milliseconds = 0
+                };
+
+                endpoint.Publish(command2).ConfigureAwait(false);
+            }
+
+            if (points.ContainsKey("Breaker_21Status"))
+            {
+                var breaker3 = points["Breaker_23Status"] as DiscretePoint;
+                var command3 = new ScadaCommandingEvent()
+                {
+                    Index = (uint)breaker3.Index,
+                    RegisterType = breaker3.RegisterType,
+                    Value = 0,
+                    Milliseconds = 0
+                };
+                endpoint.Publish(command3).ConfigureAwait(false);
+            }
+        }
+        private void Calculations()
+        {
+
+            if (points > 0 && points < 4)
+            {
+                if (pointUpdateOccures)
+                {
+                    ChangeStrategy();
+                }
+
+                var forecastResult = new CeForecast();
+                var area = GetSurfaceArea();
+                var weatherForecast = weatherAPI.GetResultsForNext6Hours();
+                var weather = new List<double>();
+                weatherForecast.ForEach(x => weather.Add(x * area));
+                float current = GetCurrentFluidLevel();
+
+                for (int i = 0; i < weatherForecast.Count; i++)
+                {
+                    ChangeStrategy();
+                    for (int j = 0; j < 4; j++)
+                    {
+                        var income = ((float)weather[i]) / 4;
+                        current += income;
+
+                        if (skip && i == 0)
                         {
-                            current -= (float)weather[i];
+                            current -= income;
                             skip = false;
+                            break;
                         }
-                        ChangeStrategy();
 
                         result = algorithm.Start(current);
-                        var processedResult = ProcessResults(current, result);
+                        var processedResult = ProcessResult(current, result);
                         forecastResult.Results.AddRange(processedResult);
                         current -= GetTotalFromResults(result.Genes);
                     }
-
-                    // Update and Command
-                    SendCommand(forecastResult);
-                    Update(forecastResult, weather);
-                    Thread.Sleep(10800000);
                 }
 
+                SendCommand(forecastResult);
+                Update(forecastResult, weather);
+
             }
+        }
+        private bool LevelIsOptimal(float fluidLevel)
+        {
+            var results = ReadConfiguration();
+
+            float lowerBound = results.OptimalFluidLevel * (1.0f - (results.Percetage / 100));
+            float upperBound = results.OptimalFluidLevel * (1.0f + (results.Percetage / 100));
+            bool ret = (fluidLevel <= upperBound && fluidLevel >= lowerBound);
+
+            return ret;
         }
 
         private void SendCommand(CeForecast forecastResult)
@@ -121,18 +267,6 @@ namespace CE
 
                         endpoint.Publish(command1).ConfigureAwait(false);
 
-                        if (onOff == 1)
-                        {
-                            var command3 = new ScadaCommandingEvent()
-                            {
-                                Index = (uint)breaker2.Index,
-                                RegisterType = breaker2.RegisterType,
-                                Value = 0,
-                                Milliseconds = (uint)((counter + time) * 60 * 1000)
-                            };
-
-                            endpoint.Publish(command3).ConfigureAwait(false);
-                        }
                     }
 
                     if (points.ContainsKey($"Discrete_Tap{i + 1}") && onOff == 1)
@@ -171,38 +305,79 @@ namespace CE
         private void Update(CeForecast forecastResult, List<double> weather)
         {
             CeUpdateEvent update = new CeUpdateEvent();
-            update.Income = weather;
+            update.Income = new List<double>();
+            for (int i = 0; i < weather.Count; i++)
+            {
+                double integralIncome = 0;
+                for (int j = 0; j < 4; j++)
+                {
+                    integralIncome += weather[i] / 4;
+                    update.Income.Add(integralIncome);
+                }
+            }
             update.Times = GetTimes();
             update.FluidLevel = new List<float>();
             foreach (var item in forecastResult.Results)
             {
+                update.FluidLevel.Add(item.StartFluidLevel);
                 update.FluidLevel.Add(item.EndFluidLevel);
             }
             update.Hours = new List<PumpsHours>();
             update.Flows = new List<PumpsFlows>();
 
-
+            CeGraphicalEvent graph = new CeGraphicalEvent();
+            graph.PumpsValues = new Core.Common.ServiceBus.Events.CeGraph();
+            List<List<long>> list = new List<List<long>>();
             for (int i = 0; i < points; i++)
             {
+                var macList = new List<long>();
+                long cumm = 0;
                 var hours = new PumpsHours();
                 var flows = new PumpsFlows();
                 foreach (var item in forecastResult.Results)
                 {
+
                     if (item.Pumps[i] == 1)
                     {
                         hours.Hours.Add(item.Times[i]);
                         flows.Flows.Add(item.Flows[i]);
+                        cumm += (long)(item.Times[i]);
                     }
                     else
                     {
                         hours.Hours.Add(0);
                         flows.Flows.Add(0);
                     }
+                    macList.Add(cumm);
                 }
                 update.Hours.Add(hours);
                 update.Flows.Add(flows);
+                list.Add(macList);
             }
             endpoint.Publish(update).ConfigureAwait(false).GetAwaiter().GetResult();
+            if (points == 1)
+            {
+                graph.PumpsValues.Pump1.XAxes = update.Times.ConvertAll(x => DateTime.Parse(x));
+                graph.PumpsValues.Pump1.YAxes = list[0];
+            }
+            if (points == 2)
+            {
+                graph.PumpsValues.Pump1.XAxes = update.Times.ConvertAll(x => DateTime.Parse(x));
+                graph.PumpsValues.Pump1.YAxes = list[0];
+                graph.PumpsValues.Pump2.XAxes = update.Times.ConvertAll(x => DateTime.Parse(x));
+                graph.PumpsValues.Pump2.YAxes = list[1];
+            }
+            if (points == 3)
+            {
+                graph.PumpsValues.Pump1.XAxes = update.Times.ConvertAll(x => DateTime.Parse(x));
+                graph.PumpsValues.Pump1.YAxes = list[0];
+                graph.PumpsValues.Pump2.XAxes = update.Times.ConvertAll(x => DateTime.Parse(x));
+                graph.PumpsValues.Pump2.YAxes = list[1];
+                graph.PumpsValues.Pump3.XAxes = update.Times.ConvertAll(x => DateTime.Parse(x));
+                graph.PumpsValues.Pump3.YAxes = list[2];
+            }
+            endpoint.Publish(graph).ConfigureAwait(false).GetAwaiter().GetResult();
+
         }
 
         private List<string> GetTimes()
@@ -219,26 +394,22 @@ namespace CE
             return list;
         }
 
-        private List<CeForecastResult> ProcessResults(float current, DNA<float> result)
+        private List<CeForecastResult> ProcessResult(float current, DNA<float> result)
         {
             var results = new List<CeForecastResult>();
-            float totalPerIteration = (GetTotalFromResults(result.Genes) / 4);
-            for (int i = 0; i < 4; i++)
-            {
-                var item = new CeForecastResult();
-                item.Result = result;
-                item.StartFluidLevel = current;
-                item.EndFluidLevel = current - totalPerIteration;
-                current -= totalPerIteration;
+            float totalPerIteration = GetTotalFromResults(result.Genes);
+            var item = new CeForecastResult();
+            item.Result = result;
+            item.StartFluidLevel = current;
+            item.EndFluidLevel = current - totalPerIteration;
 
-                for (int j = 0; j < result.Genes.Count(); j += 3)
-                {
-                    item.Pumps.Add(result.Genes[j]);
-                    item.Times.Add(result.Genes[j + 2]);
-                    item.Flows.Add(result.Genes[j + 1]);
-                }
-                results.Add(item);
+            for (int j = 0; j < result.Genes.Count(); j += 3)
+            {
+                item.Pumps.Add(result.Genes[j]);
+                item.Times.Add(result.Genes[j + 2]);
+                item.Flows.Add(result.Genes[j + 1]);
             }
+            results.Add(item);
             return results;
         }
 
@@ -319,12 +490,12 @@ namespace CE
             Stop();
         }
 
-        public void OnPointUpdate(int tPoints)
+        private void OnPointUpdate(object sender, int e)
         {
             pointUpdateOccures = true;
             if (points > 0)
                 skip = true;
-            points = tPoints;
+            points = e;
             Stop();
             OffSequence();
             Start();
