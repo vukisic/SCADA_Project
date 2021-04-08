ae2cd0bff25c0369623b5dc70ad48e568e2fc868
Danilo Novakovic <dakenzi97@gmail.com>
2021-04-08 21:48:24 +0200

    Created some initial files

---
+++ ServiceFabricProject/Core.Common/PubSub/Subscription.cs
@@ -0,0 +1,16 @@
+﻿namespace Core.Common.PubSub
+{
+    public class Subscription
+    {
+        public string ConnectionString { get; set; }
+        public string Topic { get; set; }
+        public string SubscriptionName { get; set; }
+
+        public Subscription()
+        {
+            ConnectionString = "Endpoint=sb://projekat.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=wJAF6hCmIfG81wU0AZVDdcUAv35T0VrEh1OaPzJhn+Y=";
+            Topic = "mainTopic";
+            SubscriptionName = "guisub";
+        }
+    }
+}
---
+++ ServiceFabricProject/SF.Common/Contracts/IPubSubAsync.cs
@@ -0,0 +1,13 @@
+﻿using Core.Common.PubSub;
+using System.ServiceModel;
+using System.Threading.Tasks;
+
+namespace SF.Common.Contracts
+{
+    [ServiceContract]
+    public interface IPubSubAsync
+    {
+        [OperationContract]
+        Task Publish(PubSubMessage message);
+    }
+}
